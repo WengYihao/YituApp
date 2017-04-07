f8ec2d217cf94784ea7c0426cd3bf4d725ccf7eb
wyh <15112360329@163.com>
2017-04-07 11:14:37 +0800

    hahha

--- app/src/main/java/com/cn/yitu/config/Constant.java
+++ app/src/main/java/com/cn/yitu/config/Constant.java
@@ -1,8 +1,15 @@
 package com.cn.yitu.config;
 
+import android.util.Log;
+
 public class Constant {
 	final public static String userIdKey= "userInfo";//保存用户信息的key
 	final public static String history = "history"; //保存的搜索历史
 	final public static String APPKEY ="1c988433bc238";//短信验证key
 	final public static String AppSecret = "7eed6dfce247f9a127eaa96500ba8772";//短信验证secret
+
+	public void aa(){
+		Log.i("111","hhhhh");
+		Log.i("111","hhhhh");
+	}
 }
