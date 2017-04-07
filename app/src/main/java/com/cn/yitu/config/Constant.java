fbdad59b69903c4e0aa2725c58f407b154a809fb
wyh <15112360329@163.com>
2017-04-07 11:16:58 +0800

    删除多余的方法

--- app/src/main/java/com/cn/yitu/config/Constant.java
+++ app/src/main/java/com/cn/yitu/config/Constant.java
@@ -7,9 +7,4 @@ public class Constant {
 	final public static String history = "history"; //保存的搜索历史
 	final public static String APPKEY ="1c988433bc238";//短信验证key
 	final public static String AppSecret = "7eed6dfce247f9a127eaa96500ba8772";//短信验证secret
-
-	public void aa(){
-		Log.i("111","hhhhh");
-		Log.i("111","hhhhh");
-	}
 }
